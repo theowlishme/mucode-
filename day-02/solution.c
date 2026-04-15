a3ff8cb4e81f95e576a50523d865df2df2c67de3
Anu Krishna H S <theowlishme@gmail.com>
2026-04-15 20:09:21 +0530

    Refactor secondLargest logic and improve input prompts
    Refactor secondLargest function to improve logic and variable initialization. Update main function for better input prompts.

--- day-02/solution.c
+++ day-02/solution.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
-int secondLargest(int arr[],int N){
-    int largest,slargest;
-    largest=arr[0];
-    slargest = -1;
-    for(int i=0;i<=N;i++){
-        if(arr[i]<largest && arr[i]>slargest)
-            slargest=arr[i];
-        else if(arr[i]>largest){
+int secondLargest(int arr[], int N)
+{
+    int largest=arr[0];
+    int slargest=arr[0];
+    int found=0;
+    for (int i=1;i<=N;i++)
+    {
+        if (arr[i]>largest)
+        {
             slargest=largest;
             largest=arr[i];
+            found=1;
         }
-       
-            
+        else if (arr[i]<largest&&arr[i]>slargest)
+        {
+            slargest=arr[i];
+            found=1;
+        }
     }
-    if(slargest == -1)
-         return -1;
+
+    if(!found)
+        return -1;
     return slargest;
-    
 }
-int main() {
-    int N,num;
-    printf("enter the number of elements in the array ; ");
-    scanf("%d",&num);
+int main()
+{
+    int num,N;
+    printf("Enter number of elements: ");
+    scanf("%d", &num);
     int arr[num];
-    printf("enter the elements ; ");
-    for(int i=0;i<num;i++){
-        scanf("%d",&arr[i]);
+    printf("Enter elements:\n");
+    for (int i = 0; i < num; i++)
+    {
+        scanf("%d", &arr[i]);
     }
-    printf("enter the index N ;");
+
+    printf("Enter index N: ");
     scanf("%d",&N);
     int result=secondLargest(arr,N);
-    printf("%d",result);
+    printf("Second largest = %d", result);
     return 0;
 }
